2978d01ac6681ee7a04e6302fa0228e2b4271a85
KDH <ehdgua01@naver.com>
2022-08-14 12:55:43 +0900

    test

--- src/jira_commit_msg.py
+++ src/jira_commit_msg.py
@@ -1,16 +1,5 @@
-import argparse
 import re
-import typing as tp
 
 TICKET_NUMBER_REGEX = re.compile(r"^[A-Z]{2,}-\d+")
 FAIL = 1
 PASS = 0
-
-
-def main(argv: tp.Sequence[str] | None = None) -> int:
-    print("test")
-    parser = argparse.ArgumentParser()
-    parser.add_argument("filename")
-    args = parser.parse_args(argv)
-    print(argv, args)
-    return FAIL
