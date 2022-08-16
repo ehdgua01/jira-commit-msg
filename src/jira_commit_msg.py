39364d60528cabeb98457732ee96553d72b5ce4e
KDH <ehdgua01@naver.com>
2022-08-17 00:41:35 +0900

    커맨드 완성

--- src/jira_commit_msg.py
+++ src/jira_commit_msg.py
@@ -1,12 +1,19 @@
+import argparse
+import pathlib
 import re
-import typing as tp
+import sys
 
 TICKET_NUMBER_REGEX = re.compile(r"^[A-Z]{2,}-\d+")
 FAIL = 1
 PASS = 0
 
 
-def main(argv: tp.Sequence[str] | None = None) -> int:
-    print("test")
-    print(argv)
-    return FAIL
+def main() -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("filename")
+    args = parser.parse_args()
+    commit_msg = pathlib.Path(args.filename).read_text()
+    if not TICKET_NUMBER_REGEX.match(commit_msg):
+        sys.stdout.write("[Aborted] The commit message should start with the jira ticket number.")
+        return FAIL
+    return PASS
